412f53d2d851bd196f8c4d4b28d89bc8cd75ad6f
morganglis <morganglisson7@gmail.com>
2022-09-25 19:23:02 -0400

    added name to home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -42,7 +42,7 @@ class HomeScreenState extends State<HomeScreen> {
                   ),
                   TableRow(
                       children: [
-                        Text("")
+                        Text("Morgan Glisson")
                       ]
                   ),
                 ],
