5d647ea107fa0adefe1f755aea4e81b36c957afd
SavannahEvonko <savannah.evonko@gmail.com>
2022-10-18 15:24:52 -0400

    Reconstructed the post template in post.dart file. Added fake data to app_data.dart file to use in post.dart

--- lib/widgets/Post/post.dart
+++ lib/widgets/Post/post.dart
@@ -1,37 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:paw_pals/constants/app_data.dart';
 
+import '../../models/post_model.dart';
 
 class Post extends StatelessWidget {
+  PostModel post = AppData.fakePost;
+
+  Post({super.key});
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: Card(
-        elevation: 10.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0)
-          ),
-          color: Colors.amber,
-          child: Container(
-            padding: EdgeInsets.all(10.0),
-            width: 500,
-            child: Column(
-              children: [
-                Text(
-              "Pet's Name",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 10,
+        left: 20,
+        right:20,
+      ),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height / 1.4,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AppData.siameseCat
                 ),
-                Image.asset('images/Rocket.jpg',width:800, height:350),
-             SizedBox(height:10.0),
-            Text(
-              'more things here',
-              //style:TextStyle(),
-            )
-
-              ],
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 4,
+                    blurRadius: 4,
+                    offset: Offset(3, 3),
+                  ),
+                ]
+              ),
             ),
-          )
-
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color.fromARGB(200, 0, 0, 0),
+                      Color.fromARGB(0, 0, 0, 0),
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+              ),
+            Positioned(
+              bottom: 30,
+              left: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('${AppData.fakePost.petName}, 4',
+                  style: Theme.of(context).textTheme.headline3!.copyWith(
+                    color: Colors.white,
+                  ),
+                  ),
+                  Text('Male, Wilmington',
+                    style: Theme.of(context).textTheme.headline4!.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Row(children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 8, right: 8,
+                      ),
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AppData.siameseCat,
+                            fit: BoxFit.cover
+                          ),
+                        borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                  ],)
+                ]
+              ),
+            )
+          ],
         ),
-      );
+      ),
+    );
+
   }
 
 
