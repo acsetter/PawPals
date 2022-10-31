683e351ab629ab1333bc15c5b5a5ed21176cabdb
SavannahEvonko <savannah.evonko@gmail.com>
2022-10-31 10:17:43 -0400

    Added about me and description. Added cat friendly, dog friendly, kid friendly labels. Picture from post with name, age, gender and location will not appear. No overflow issues.

--- lib/widgets/Post/detailedPost.dart
+++ lib/widgets/Post/detailedPost.dart
@@ -5,91 +5,171 @@ import '../../models/post_model.dart';
 class DetailedPost extends StatelessWidget{
   final PostModel post = AppData.fakePost;
 
-  DetailedPost({super.key});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
 
     return Scaffold(
 
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.white
-        ),
-        child: Stack(
-          children: [
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              color: Colors.white
+          ),
+          child: Stack(
 
-            Column(
+            children: [
+
+              Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
 
                   Container(
-                    height: MediaQuery.of(context).size.height,
+
+                    height: MediaQuery.of(context).size.height*0.36,
                     width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.only(left:35),
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage("assets/images/siamese_cat.jpg"),
+                            image: AssetImage("assets/images/siamese_cat.png"),
                             fit: BoxFit.fill
                         )
                     ),
+
                   ),
                   SizedBox(
-                    height: 20,
+                    height: 10,
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+
+                      Container(
+                        margin: EdgeInsets.only(left:30),
+                        child: Text('${AppData.fakePost.petName}',
+                          style: Theme.of(context).textTheme.headline4!.copyWith(
+                            color: Colors.white,
+                          ),),
+
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(left: 30),
+                    child: Text('${AppData.fakePost.petName}',
+                      style: Theme.of(context).textTheme.headline4!.copyWith(
+                        color: Colors.white,
+                      ),),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+
+                  SizedBox(
+                    height: 15,
                   ),
                   Container(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.only(left:35),
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage("assets/images/siamese_cat.jpg"),
-                            fit: BoxFit.fill
-                        )
-                    ),
-                  ),
-                  Text('${AppData.fakePost.petName}',
-                    style: Theme.of(context).textTheme.headline4!.copyWith(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text('4, Male, Wilmington',
-                    style: Theme.of(context).textTheme.headline5!.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ]
-            ),
+                    margin: EdgeInsets.only(left:30),
+                    child: Text("About Me", style: TextStyle(fontFamily: "Proxima-Nova-Bold",
+                        fontSize: 20, color: Colors.black, fontWeight: FontWeight.w400
+                    ),),
 
-            Positioned(
-              left: MediaQuery.of(context).size.width,
-              top: MediaQuery.of(context).size.height,
-              child: Icon(
-                Icons.arrow_back,
-                size: 20,
-                color: Colors.black,
+                  ),
+
+                  SizedBox(
+                    height: 5,
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(left: 30),
+                    width: MediaQuery.of(context).size.width*0.85,
+                    child: Text("Whiskers is looking for a furrever home. He loves to cuddle and lay in the sun.", style: TextStyle(fontFamily: "ProximaNova-Regular",
+                        fontSize: 14, color: Colors.black, fontWeight: FontWeight.w300
+                    ),),
+                  ),
+
+                  SizedBox(
+                    height: 5,
+                  ),
+
+                  Container(
+                    width: MediaQuery.of(context).size.width*0.9,
+                    margin: EdgeInsets.only(left: 30,right: 30,),
+                    child: Wrap(
+
+                      children: [
+
+                        Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15.0),
+                                color: Color(0xFF7FE9F6).withOpacity(0.25)
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 6, right: 6, top: 6, bottom: 6 ),
+                              child: Text("Cat Friendly", style: TextStyle(fontFamily: "ProximaNova-Regular",
+                                  fontSize: 14, color: Colors.black, fontWeight: FontWeight.w400
+                              ),),
+                            )
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15.0),
+                                color: Color(0xFF33C0FF).withOpacity(0.25)
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 6, right: 6, top: 6, bottom: 6 ),
+                              child: Text("Dog Friendly", style: TextStyle(fontFamily: "ProximaNova-Regular",
+                                  fontSize: 14, color: Colors.black, fontWeight: FontWeight.w400
+                              ),),
+                            )
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15.0),
+                                color: Color(0xFFFF9933).withOpacity(0.25)
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 6, right: 6, top: 6, bottom: 6),
+                              child: Text("Kid Friendly", style: TextStyle(fontFamily: "ProximaNova-Regular",
+                                  fontSize: 14, color: Colors.black, fontWeight: FontWeight.w400
+                              ),),
+                            )
+                        ),
+                      ],
+
+                    ),
+                  ),
+
+                ],
+
               ),
-            ),
-            Positioned(
-                right: MediaQuery.of(context).size.width,
-                top: MediaQuery.of(context).size.height,
-                child: Icon(
-                  Icons.arrow_back,
-                  size: 20,
-                  color: Colors.black,
-                ),
-            ),
-          ]
-
-        ),
 
 
-      )
+            ],
+
+          ),
+        )
+
     );
+
   }
+
 
 
 }
