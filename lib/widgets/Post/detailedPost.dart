c084861f7250d8acb592b3378b9df293459574dc
SavannahEvonko <savannah.evonko@gmail.com>
2022-10-31 13:07:28 -0400

    Fixed picture and text display issue.

--- lib/widgets/Post/detailedPost.dart
+++ lib/widgets/Post/detailedPost.dart
@@ -14,31 +14,26 @@ class DetailedPost extends StatelessWidget{
         body: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               color: Colors.white
           ),
           child: Stack(
-
             children: [
-
               Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-
                   Container(
-
-                    height: MediaQuery.of(context).size.height*0.36,
+                    height: MediaQuery.of(context).size.height*0.40,
                     width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage("assets/images/siamese_cat.png"),
+                            image: AssetImage("assets/images/siamese_cat.jpg"),
                             fit: BoxFit.fill
                         )
                     ),
-
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
 
@@ -48,60 +43,62 @@ class DetailedPost extends StatelessWidget{
                     children: [
 
                       Container(
-                        margin: EdgeInsets.only(left:30),
+                        margin: const EdgeInsets.only(left:30),
                         child: Text('${AppData.fakePost.petName}',
                           style: Theme.of(context).textTheme.headline4!.copyWith(
-                            color: Colors.white,
+                            color: Colors.black,
                           ),),
 
                       ),
                     ],
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 5,
                   ),
 
                   Container(
-                    margin: EdgeInsets.only(left: 30),
-                    child: Text('${AppData.fakePost.petName}',
-                      style: Theme.of(context).textTheme.headline4!.copyWith(
-                        color: Colors.white,
-                      ),),
+                    margin: const EdgeInsets.only(left: 30),
+                    child: Text('4, Male, Wilmington',
+                      style: Theme.of(context).textTheme.headline5!.copyWith(
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 5,
                   ),
 
-                  SizedBox(
+                  const SizedBox(
                     height: 15,
                   ),
                   Container(
-                    margin: EdgeInsets.only(left:30),
-                    child: Text("About Me", style: TextStyle(fontFamily: "Proxima-Nova-Bold",
+                    margin: const EdgeInsets.only(left:30),
+                    child: const Text("About Me", style: TextStyle(fontFamily: "Proxima-Nova-Bold",
                         fontSize: 20, color: Colors.black, fontWeight: FontWeight.w400
                     ),),
 
                   ),
 
-                  SizedBox(
+                  const SizedBox(
                     height: 5,
                   ),
 
                   Container(
-                    margin: EdgeInsets.only(left: 30),
+                    margin: const EdgeInsets.only(left: 30),
                     width: MediaQuery.of(context).size.width*0.85,
-                    child: Text("Whiskers is looking for a furrever home. He loves to cuddle and lay in the sun.", style: TextStyle(fontFamily: "ProximaNova-Regular",
+                    child: const Text("Whiskers is looking for a furrever home. He loves to cuddle and lay in the sun.", style: TextStyle(fontFamily: "ProximaNova-Regular",
                         fontSize: 14, color: Colors.black, fontWeight: FontWeight.w300
                     ),),
                   ),
 
-                  SizedBox(
-                    height: 5,
+                  const SizedBox(
+                    height: 10,
                   ),
 
                   Container(
                     width: MediaQuery.of(context).size.width*0.9,
-                    margin: EdgeInsets.only(left: 30,right: 30,),
+                    margin: const EdgeInsets.only(left: 30,right: 30,),
                     child: Wrap(
 
                       children: [
@@ -110,41 +107,41 @@ class DetailedPost extends StatelessWidget{
                             height: 30,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(15.0),
-                                color: Color(0xFF7FE9F6).withOpacity(0.25)
+                                color: const Color(0xFF7FE9F6).withOpacity(0.25)
                             ),
-                            child: Padding(
+                            child: const Padding(
                               padding: EdgeInsets.only(left: 6, right: 6, top: 6, bottom: 6 ),
                               child: Text("Cat Friendly", style: TextStyle(fontFamily: "ProximaNova-Regular",
                                   fontSize: 14, color: Colors.black, fontWeight: FontWeight.w400
                               ),),
                             )
                         ),
-                        SizedBox(
-                          width: 5,
+                        const SizedBox(
+                          width: 10,
                         ),
                         Container(
                             height: 30,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(15.0),
-                                color: Color(0xFF33C0FF).withOpacity(0.25)
+                                color: const Color(0xFF33C0FF).withOpacity(0.25)
                             ),
-                            child: Padding(
+                            child: const Padding(
                               padding: EdgeInsets.only(left: 6, right: 6, top: 6, bottom: 6 ),
                               child: Text("Dog Friendly", style: TextStyle(fontFamily: "ProximaNova-Regular",
                                   fontSize: 14, color: Colors.black, fontWeight: FontWeight.w400
                               ),),
                             )
                         ),
-                        SizedBox(
-                          width: 5,
+                        const SizedBox(
+                          width: 10,
                         ),
                         Container(
                             height: 30,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(15.0),
-                                color: Color(0xFFFF9933).withOpacity(0.25)
+                                color: const Color(0xFFFF9933).withOpacity(0.25)
                             ),
-                            child: Padding(
+                            child: const Padding(
                               padding: EdgeInsets.only(left: 6, right: 6, top: 6, bottom: 6),
                               child: Text("Kid Friendly", style: TextStyle(fontFamily: "ProximaNova-Regular",
                                   fontSize: 14, color: Colors.black, fontWeight: FontWeight.w400
