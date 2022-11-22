d72baf1167a97444b34b900aadc1f33962475b9e
Morgan Glisson <morganglisson7@gmail.com>
2022-11-22 09:56:10 -0500

    Still trying to get login button to only register on the profile of the user that is currently logged in

--- lib/widgets/bars/our_app_bar_profile.dart
+++ lib/widgets/bars/our_app_bar_profile.dart
@@ -1,11 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '../../controllers/app_user.dart';
-import '../../models/user_model.dart';
-import '../../screens/profile/profile_screen.dart';
-import '../../services/firestore_service.dart';
-import '../profile/profile_builder.dart';
 import 'logout_popup.dart';
 
 
